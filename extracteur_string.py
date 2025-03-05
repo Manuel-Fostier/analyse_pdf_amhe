9679367464d784e30f4684d95b41ba3cd92e48e1
Manuel <manuel.fostier@gmail.com>
2025-03-05 23:02:52 +0100

    boucle for refactorée

--- extracteur_string.py
+++ extracteur_string.py
@@ -47,51 +47,60 @@ def extract_text_elements(pdf_path, search_string, page_range):
     chapter_index = 1
     paragraphs = []
     text_content = ""
+    previous_word_size = 0
+    word_text = ""
 
     for i, word in enumerate(words):
-        if word['text'] != ' ' :
-            if round(word['size']) == 80:
-                if text_content.strip() and current_title == "":
-                    title_index += 1
-                current_title += word['text']
+        word_text = word['text']
+        if word_text != ' ' :
+            current_word_size = round(word['size'])
 
-            elif round(word['size']) == 25:
-                if text_content.strip() and current_title_1 == "":
-                    title_1_index += 1
-                current_title_1 += word['text']
-
-            elif round(word['size']) == 20:
-                if text_content.strip() and current_chapter == "":
-                    # Split the accumulated text into paragraphs
-                    paragraphs_text = re.split(r'\n(?=[A-ZÉÈÊËÀÂÎÔÛÜÇ])', text_content)
-                    text_content = ""
-                    for para_index, para in enumerate(paragraphs_text, start=1):
-                        # Create a Paragraph object and associate it with the most recent Chapter
-                        paragraphs.append(Paragraph(para, para_index, chapters[chapter_index-1]))
-                    chapter_index += 1
-                current_chapter += word['text']
-            else:
-                if current_title.strip():
-                    # Create a Title object
+            # Set previous_word_size value on first run
+            if previous_word_size == 0 :
+                previous_word_size = current_word_size
+            
+            if(current_word_size != previous_word_size) :
+                if previous_word_size == 80:
                     titles.append(Title(current_title.strip(), title_index))
+                    title_index += 1
                     current_title = ""
 
-                if current_title_1.strip():
-                    # Create a Title 1 object
+                elif previous_word_size == 25 :
                     titles_1.append(Title1(current_title_1.strip(), title_1_index, titles[title_index-1]))
+                    title_1_index += 1
                     current_title_1 = ""
 
-                if current_chapter.strip():
-                    # Create a Chapter object
+                elif previous_word_size == 20 :
                     chapters.append(Chapter(current_chapter.strip(), chapter_index, titles_1[title_1_index-1]))
+                    chapter_index += 1
                     current_chapter = ""
 
+                else :
+                    # Split the accumulated text into paragraphs
+                    paragraphs_text = re.split(r'\n(?=[A-ZÉÈÊËÀÂÎÔÛÜÇ])', text_content)
+                    # Associate each Paragraph to current Chapter
+                    for para_index, para in enumerate(paragraphs_text, start=1):
+                        paragraphs.append(Paragraph(para, para_index, chapters[chapter_index-1]))
+                    text_content = ""                          
+
+            if current_word_size == 80:
+                current_title += word_text
+
+            elif current_word_size == 25:
+                current_title_1 += word_text
+
+            elif current_word_size == 20:
+                current_chapter += word_text
+
+            else:
                 # Add a newline if the difference in doctop between the current and next word is greater than 3
                 if i < len(words) - 1 and abs(words[i + 1]['doctop'] - word['doctop']) > 20: #diff entre deux lignes 14, entre deux paragraphes 25,9
-                    text_content += word['text']
+                    text_content += word_text
                     text_content += '\n'
                 else:
-                    text_content += word['text']
+                    text_content += word_text
+
+            previous_word_size = current_word_size
 
     # Filtrer les paragraphes contenant le search_string
     filtered_paragraphs = [para for para in paragraphs if search_string in para.text]
